1f86036ca4879068b38134080b966ee04ab5e590
fatih.cetinkaya <fatih.cetinkaya@FTH>
2019-03-14 14:30:11 +0300

    Initial commit

---
+++ WebAPI/WebAPI/Models/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class AuthContext:IdentityDbContext
+    {
+        public AuthContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    }
+}
